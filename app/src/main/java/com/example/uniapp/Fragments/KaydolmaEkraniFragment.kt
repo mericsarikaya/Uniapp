86c385a68038b36ec63c25b86ff99f426eb0f106
mericsarikaya <mericsarikaya83@gmail.com>
2025-12-06 16:54:23 +0300

    Added DAO function for login verification

---
+++ app/src/main/java/com/example/uniapp/Fragments/KaydolmaEkraniFragment.kt
@@ -0,0 +1,62 @@
+package com.example.uniapp.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import com.example.uniapp.Data.KisiTablosu.KisiTablo
+import com.example.uniapp.Data.KisiTablosu.KisiViewModel
+import com.example.uniapp.databinding.FragmentKaydolmaEkraniBinding
+import com.example.uniapp.R
+
+class KaydolmaEkraniFragment : Fragment() {
+    private lateinit var binding: FragmentKaydolmaEkraniBinding
+    private lateinit var mkisiviewmodel: KisiViewModel
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentKaydolmaEkraniBinding.inflate(inflater,container, false)
+        mkisiviewmodel = ViewModelProvider(this).get(KisiViewModel::class.java)
+        binding.bilgiKaydet.setOnClickListener {view ->
+            veritabaninaKaydet(view)
+        }
+        return binding.root
+    }
+
+
+    private fun veritabaninaKaydet(view: View){
+        val isim = binding.isimGirdi.text.toString()
+        val soyisim = binding.soyisimGirdi.text.toString()
+        val mail = binding.mailGirdi.text.toString()
+        val telefon = binding.telefonGirdi.text.toString()
+        val dogum = binding.dogumGirdi.text.toString()
+        val okul = binding.okulGirdi.text.toString()
+        val bolum = binding.bolumGirdi.text.toString()
+        val sifre = binding.sifreGirdi.text.toString()
+
+
+        if (bosKontrol(isim,soyisim,mail,telefon,dogum,okul,bolum,sifre)){
+            val kullanici = KisiTablo(0, isim, soyisim, mail, telefon, dogum,sifre)
+            mkisiviewmodel.kisiEkle(kullanici)
+            Toast.makeText(requireContext(), "Başarıyla Eklendi!", Toast.LENGTH_LONG).show()
+            Navigation.findNavController(view).navigate(R.id.giris_geri_gecis)
+        }
+        else{
+            Toast.makeText(requireContext(), "Lütfen boş alanları doldurunuz!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun bosKontrol(isim: String, soyisim: String, mail:String, telefon: String, dogum: String, okul: String, bolum: String, sifre: String): Boolean{
+                return isim.isNotEmpty() &&
+                        soyisim.isNotEmpty() &&
+                        mail.isNotEmpty() &&
+                        telefon.isNotEmpty() &&
+                        dogum.isNotEmpty() &&
+                        okul.isNotEmpty() &&
+                        bolum.isNotEmpty() &&
+                        sifre.isNotEmpty()
+    }
+
+}
